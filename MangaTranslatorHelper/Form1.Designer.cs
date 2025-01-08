c352bd3060814498a6d0449fc7d1765131bde750
artBETEP <100296058+artBETEP@users.noreply.github.com>
2025-01-08 10:01:21 +0500

    Initial commit

---
+++ MangaTranslatorHelper/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace MangaTranslatorHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            button2 = new Button();
+            pictureBox1 = new PictureBox();
+            listView1 = new ListView();
+            textBox1 = new TextBox();
+            button3 = new Button();
+            imageList1 = new ImageList(components);
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(29, 22);
+            button1.Name = "button1";
+            button1.Size = new Size(185, 48);
+            button1.TabIndex = 0;
+            button1.Text = "Open Image";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(221, 22);
+            button2.Name = "button2";
+            button2.Size = new Size(211, 48);
+            button2.TabIndex = 1;
+            button2.Text = "Open Mark File";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(457, 35);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(800, 800);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            pictureBox1.Paint += pictureBox1_Paint;
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
+            pictureBox1.MouseUp += pictureBox1_MouseUp;
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(37, 594);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(388, 590);
+            listView1.TabIndex = 3;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(37, 360);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(388, 182);
+            textBox1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(221, 77);
+            button3.Name = "button3";
+            button3.Size = new Size(211, 46);
+            button3.TabIndex = 5;
+            button3.Text = "Save Mark FIle";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth8Bit;
+            imageList1.ImageSize = new Size(16, 16);
+            imageList1.TransparentColor = Color.Transparent;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            ClientSize = new Size(1984, 1278);
+            Controls.Add(button3);
+            Controls.Add(textBox1);
+            Controls.Add(listView1);
+            Controls.Add(pictureBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Manga Translator Helper";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private PictureBox pictureBox1;
+        private ListView listView1;
+        private TextBox textBox1;
+        private Button button3;
+        private ImageList imageList1;
+        private ContextMenuStrip contextMenuStrip1;
+    }
+}
---
+++ MangaTranslatorHelper/Form1.cs
@@ -0,0 +1,216 @@
+using System.Text;
+
+namespace MangaTranslatorHelper
+{
+    public partial class Form1 : Form
+    {
+        private bool isDrawing = false;
+        private Point startPoint;
+        private Rectangle currentRectangle;
+        private List<Annotation> annotations = new List<Annotation>();
+        private Annotation selectedAnnotation = null;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            contextMenuStrip1 = new ContextMenuStrip();
+            contextMenuStrip1.Items.Add("Edit", null, EditAnnotation);
+            contextMenuStrip1.Items.Add("Delete", null, DeleteAnnotation);
+
+            pictureBox1.ContextMenuStrip = contextMenuStrip1;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "Image files|*.png;*.jpeg;*.jpg;*.bmp;*.gif|All files(*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                annotations.Clear();
+                pictureBox1.Image = Image.FromFile(ofd.FileName);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                selectedAnnotation = annotations.FirstOrDefault(a => a.Area.Contains(e.Location));
+
+                if (selectedAnnotation != null)
+                {
+                    //MessageBox.Show($"Annotation selected: {selectedAnnotation.Label}");
+                    pictureBox1.Invalidate();
+                    return;
+                }
+
+                isDrawing = true;
+                startPoint = e.Location;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                int x = Math.Min(startPoint.X, e.X);
+                int y = Math.Min(startPoint.Y, e.Y);
+                int width = Math.Abs(startPoint.X - e.X);
+                int height = Math.Abs(startPoint.Y - e.Y);
+                currentRectangle = new Rectangle(x, y, width, height);
+
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                isDrawing = false;
+
+                var label = PromptForLabel();
+                //var label = "Test text";
+
+                if (!string.IsNullOrEmpty(label))
+                {
+                    annotations.Add(new Annotation
+                    {
+                        Area = currentRectangle,
+                        Label = label
+                    });
+
+                    //MessageBox.Show($"Area saved: {currentRectangle}, Mark: {label}");
+                }
+
+                currentRectangle = Rectangle.Empty;
+
+            }
+            else if (e.Button == MouseButtons.Right && selectedAnnotation != null)
+            {
+                contextMenuStrip1.Show(pictureBox1, e.Location);
+                isDrawing = false;
+            }
+
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (isDrawing)
+            {
+                using (Pen pen = new Pen(Color.Red, 2))
+                {
+                    e.Graphics.DrawRectangle(pen, currentRectangle);
+                }
+            }
+
+            foreach (var annotation in annotations)
+            {
+                using (Pen pen = new Pen(Color.Blue, 2))
+                {
+                    e.Graphics.DrawRectangle(pen, annotation.Area);
+                }
+
+                using (Font font = new Font("Arial", 10))
+                using (Brush brush = new SolidBrush(Color.Blue))
+                {
+                    e.Graphics.DrawString(annotation.Label, font, brush, annotation.Area);
+                }
+            }
+
+            if (selectedAnnotation != null)
+            {
+                using (Pen pen = new Pen(Color.Red, 2))
+                {
+                    e.Graphics.DrawRectangle(pen, selectedAnnotation.Area);
+                }
+            }
+        }
+
+        private string PromptForLabel()
+        {
+            using (var prompt = new Form())
+            {
+                prompt.Width = 500;
+                prompt.Height = 500;
+                prompt.Text = "Enter text";
+
+                var textLabel = new Label() { Left = 20, Top = 15, Text = "Mark:" };
+                var inputBox = new TextBox() { Left = 20, Top = 50, Width = 400, Height = 300, Multiline = true };
+
+                var confirmation = new Button() { Text = "OK", Left = 180, Top = 350, Width = 80, Height = 80 };
+
+                confirmation.Click += (sender, e) => { prompt.DialogResult = DialogResult.OK; prompt.Close(); };
+
+                prompt.Controls.Add(textLabel);
+                prompt.Controls.Add(inputBox);
+                prompt.Controls.Add(confirmation);
+                prompt.AcceptButton = confirmation;
+
+                return prompt.ShowDialog() == DialogResult.OK ? inputBox.Text : string.Empty;
+            }
+        }
+
+        private void EditAnnotation(object sender, EventArgs e)
+        {
+            if (selectedAnnotation != null)
+            {
+                string newLabel = PromptForLabel();
+                if (!string.IsNullOrEmpty(newLabel))
+                {
+                    selectedAnnotation.Label = newLabel;
+                    pictureBox1.Invalidate();
+                }
+            }
+        }
+
+        private void DeleteAnnotation(object sender, EventArgs e)
+        {
+            if (selectedAnnotation != null)
+            {
+                annotations.Remove(selectedAnnotation);
+                selectedAnnotation = null;
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            sfd.Title = "Save marking data";
+            sfd.Filter = "Text files|*.txt|All files(*.*)|*.*";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                SaveMarkingToFile(sfd.FileName);
+            }
+        }
+
+        private void SaveMarkingToFile(string fileName)
+        {
+            if (annotations.Any())
+            {
+
+                var sb = new StringBuilder();
+
+                foreach (var annotation in annotations)
+                {
+                    sb.AppendLine($"[{annotation.Area.Left},{annotation.Area.Top}][{annotation.Area.Width},{annotation.Area.Height}]");
+                    sb.AppendLine(annotation.Label);
+                }
+
+                File.WriteAllText(fileName, sb.ToString());
+            }
+        }
+    }
+}
